208f10bd08a6109ebcc0379fbd318e2f626cdebd
Salief Lewis <salieflewis@gmail.com>
2022-02-06 17:36:36 -0500

    third commit

--- contracts/Captain.sol
+++ contracts/Captain.sol
@@ -3,50 +3,98 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 // import "./Conscript.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Captain {
 
-    address private owner;
+address private owner;
   
-    constructor() payable {
-        owner = msg.sender;
-    }
+constructor() payable {
+owner = msg.sender;
+}
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
+event newConscriptLog(address newConscript);
 
-    Conscript[] public conscripts;
+modifier onlyOwner() {
+ require(msg.sender == owner);
+ _;
+}
 
-    function newConscript(string memory _initName) external onlyOwner {
-        Conscript conscript = new Conscript(_initName, address(this));
-        conscripts.push(conscript);
-    }
+Conscript[] public conscripts;
+
+function newConscript(string memory _initName) external onlyOwner {
+
+Conscript conscript = new Conscript(_initName, address(this));
+//conscripts.push(conscript);
+//**Events emitted wihin functions**
+emit newConscriptLog(address(conscript));
+}
+
+//**JS CODE 4 LISTENING TO EVENTS & UPDATING UI**
+//contract = web3.eth.contract(abi).at(address);
+//contract.newConscriptLog(function (err, result) {
+//if (err) {
+//return error(err);
+//}
+
+//log("New Address:" + result.args.newConscript);
+//getNewConscript(_initName);
+//});
+
+//getCount();
+
 }
 
 contract Conscript {
 
-    string public initName;
-    address public owner;
+string public initName;
+address public newConscript;
 
-    constructor (string memory _initName, address _owner) {
-        initName = _initName;
-        owner = _owner;
-    }
+constructor (string memory _initName, address _newConscript){
+initName = _initName;
+newConscript = _newConscript;
+}
 
-    modifier onlyCaptain() {
-        require(msg.sender == owner);
-        _;
-    }
+modifier onlyCaptain() {
+require(msg.sender == newConscript);
+_;
+}
 
-    // Enables the contract to receive ether
-    function receiveDonation(uint256 amount) public payable {
-        require (msg.value == amount);
-    }
+// Enables the contract to receive ether
+function receiveDonation(uint256 amount) public payable {
+}
 
-    // Returns the ether balance of the contract
-    function getBalance() public view returns (uint256) {
-        return address(this).balance;
-    }
+// Returns the ether balance of the contract
+function getBalance() public view returns (uint256) {
+return address(this).balance;
+}
+
+}
+
+contract BalanceInUSD {
+
+AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Kovan
+     * Aggregator: ETH/USD
+     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+     */
+constructor() {
+priceFeed = AggregatorV3Interface(address(0x9326BFA02ADD2366b30bacB125260Af641031331));
+}
+
+function getThePrice() public view returns (int) {
+(
+    , 
+    int price,
+    ,
+    ,
+    
+) = priceFeed.latestRoundData();
+return (price / 10**8);
+}
+    
+uint current_price = uint(getThePrice());
+
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,8 @@ require('hardhat-deploy');
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-  solidity: "0.7.3",
+  // solidity: "0.7.3",
+  solidity: "0.8.0",
   networks: {
     mumbai: {
       url: process.env.STAGING_ALCHEMY_API,
